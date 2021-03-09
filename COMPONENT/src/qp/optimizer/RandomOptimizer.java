54130e2fb5c033e24cecb5a64daa51d2178c2f4a
linyutinglyt <e0332737@u.nus.edu>
2021-03-09 20:55:27 +0800

    implement blocknestedjoin

--- COMPONENT/src/qp/optimizer/RandomOptimizer.java
+++ COMPONENT/src/qp/optimizer/RandomOptimizer.java
@@ -55,6 +55,11 @@ public class RandomOptimizer {
                     nj.setRight(right);
                     nj.setNumBuff(numbuff);
                     return nj;
+                case JoinType.BLOCKNESTED:
+                    BlockNestedJoin bnj = new BlockNestedJoin((Join) node);
+                    bnj.setLeft(left);
+                    bnj.setRight(right);
+                    bnj.setNumBuff(numbuff);
                 default:
                     return node;
             }
